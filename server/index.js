02a9a69920e16dca3de1560b55f075693eb174ae
anushak1302 <anusha_karve_ece@moderncoe.edu.in>
2025-04-17 03:04:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,13 +7,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("API is running"));
+// Basic route
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("DB connection error:", err));
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("DB connection error:", err));
 
+// Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(\`Server running on port \${PORT}\`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
